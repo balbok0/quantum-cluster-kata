fde4d12aa3a6126a2358fd7daf1e5fd0649d0b17
balbok0 <jakubflpk@gmail.com>
2019-03-10 21:16:27 -0700

    Data Generation added. Quant find max subroutine should be working if distance oracles are working

---
+++ generate_dataset.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# Two binomial datapoints - far away
+a = np.random.binomial(15, 2.0/15.0, 7)
+b = np.random.binomial(15, 12.0/15.0, 8)
+
+data = np.concatenate((a, b))
+np.savetxt('data_2_peak_binom_far.txt', data, delimiter=';')
+
+# Two binomial datapoints - close by
+a = np.random.binomial(15, 5.0/15.0, 7)
+b = np.random.binomial(15, 8.0/15.0, 8)
+
+data = np.concatenate((a, b))
+np.savetxt('data_2_peak_binom_close.txt', data, delimiter=';')
+
+# Geometric datapoints - far away
+a = np.random.geometric(1.5/15.0, 7)
+b = np.random.geometric(5.0/15.0, 8)
+a[a > 15] = 15
+b[b > 15] = 15
+
+data = np.concatenate((a, b))
+np.savetxt('data_geom_far.txt', data, delimiter=';')
