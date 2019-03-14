dfa17208123773ce51164a4f661fe9d8d497fc60
balbok0 <jakubflpk@gmail.com>
2019-03-13 18:20:40 -0700

    Fixed dataset issues

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,14 +5,14 @@ a = np.random.binomial(15, 2.0/15.0, 8)
 b = np.random.binomial(15, 12.0/15.0, 8)
 
 data = np.concatenate((a, b))
-np.savetxt('data_2_peak_binom_far.txt', data, delimiter=';')
+np.savetxt('data_2_peak_binom_far.txt', data, fmt='%d', delimiter=';')
 
 # Two binomial datapoints - close by
 a = np.random.binomial(15, 5.0/15.0, 8)
 b = np.random.binomial(15, 8.0/15.0, 8)
 
 data = np.concatenate((a, b))
-np.savetxt('data_2_peak_binom_close.txt', data, delimiter=';')
+np.savetxt('data_2_peak_binom_close.txt', data, fmt='%d', delimiter=';')
 
 # Geometric datapoints - far away
 a = np.random.geometric(1.5/15.0, 8)
@@ -21,4 +21,4 @@ a[a > 15] = 15
 b[b > 15] = 15
 
 data = np.concatenate((a, b))
-np.savetxt('data_geom_far.txt', data, delimiter=';')
+np.savetxt('data_geom_far.txt', data, fmt='%d', delimiter=';')
